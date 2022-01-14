afdd4b228438a9a8382de5a2d542bc43949e0c5e
jankrynauw <jan@alis.capital>
2022-01-14 11:16:33 +0200

    fix: updated ord name to alis-x

--- alis/internal/cmd/root.go
+++ alis/internal/cmd/root.go
@@ -31,7 +31,7 @@ var (
 	ptermInput           pterm.PrefixPrinter
 )
 
-const VERSION = "3.0.8"
+const VERSION = "3.0.9"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
