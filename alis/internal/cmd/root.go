8667d4bfe34dbb4299d1c2e1a3a47dbdae3c2434
jankrynauw <jan@alis.capital>
2022-01-14 11:15:05 +0200

    chore: ignore intellij .idea folder.

--- alis/internal/cmd/root.go
+++ alis/internal/cmd/root.go
@@ -31,7 +31,7 @@ var (
 	ptermInput           pterm.PrefixPrinter
 )
 
-const VERSION = "3.0.7"
+const VERSION = "3.0.8"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
